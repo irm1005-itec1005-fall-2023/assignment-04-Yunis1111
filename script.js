abd5a28a420edfcccb38be45ea7e66603a640f78
Yunis Mohamud <yunismohamud4@gmail.com>
2023-11-29 19:26:52 -0500

    360

--- script.js
+++ script.js
@@ -9,21 +9,12 @@ function addTodo() {
     if (newTodo) {
         let listItem = document.createElement('li');
         listItem.textContent = newTodo;
-        
-        let deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete';
-        deleteBtn.className = 'delete-btn';
-        deleteBtn.onclick = function() {
-            listItem.remove();
-            saveTodos();
-        };
 
+        let completedBtn = createCompletedButton();
+        listItem.appendChild(completedBtn);
+
+        let deleteBtn = createDeleteButton();
         listItem.appendChild(deleteBtn);
-
-        listItem.addEventListener('click', function() {
-            listItem.classList.toggle('completed');
-            saveTodos();
-        });
 
         document.getElementById('todo-list').appendChild(listItem);
         todoInput.value = '';
@@ -31,48 +22,59 @@ function addTodo() {
     }
 }
 
+function createCompletedButton() {
+    let completedBtn = document.createElement('button');
+    completedBtn.textContent = 'Mark as Completed';
+    completedBtn.className = 'completed-btn';
+    completedBtn.addEventListener('click', function(event) {
+        event.target.parentElement.classList.toggle('completed');
+        saveTodos();
+    });
+    return completedBtn;
+}
+
+function createDeleteButton() {
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.className = 'delete-btn';
+    deleteBtn.addEventListener('click', function(event) {
+        event.target.parentElement.remove();
+        saveTodos();
+    });
+    return deleteBtn;
+}
+
 function clearTodos() {
-    let todoList = document.getElementById('todo-list');
-    todoList.innerHTML = '';
+    document.getElementById('todo-list').innerHTML = '';
     saveTodos();
 }
 
 function loadTodos() {
     let todos = JSON.parse(localStorage.getItem('todos')) || [];
-    for (let todo of todos) {
+    todos.forEach(todo => {
         let listItem = document.createElement('li');
         listItem.textContent = todo.text;
 
-        let deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete';
-        deleteBtn.className = 'delete-btn';
-        deleteBtn.onclick = function() {
-            listItem.remove();
-            saveTodos();
-        };
+        let completedBtn = createCompletedButton();
+        listItem.appendChild(completedBtn);
 
+        let deleteBtn = createDeleteButton();
         listItem.appendChild(deleteBtn);
 
         if (todo.completed) {
             listItem.classList.add('completed');
         }
 
-        listItem.addEventListener('click', function() {
-            listItem.classList.toggle('completed');
-            saveTodos();
-        });
-
         document.getElementById('todo-list').appendChild(listItem);
-    }
+    });
 }
 
 function saveTodos() {
     let todos = [];
     document.querySelectorAll('#todo-list li').forEach(item => {
-        todos.push({
-            text: item.firstChild.textContent,
-            completed: item.classList.contains('completed')
-        });
+        let todoText = item.firstChild.textContent;
+        let isCompleted = item.classList.contains('completed');
+        todos.push({ text: todoText, completed: isCompleted });
     });
     localStorage.setItem('todos', JSON.stringify(todos));
 }
